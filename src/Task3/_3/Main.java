353e7ba7c5761afdde192f7744b5000b976356f0
Anton K <komarov@firecode.ru>
2024-02-26 17:29:27 +0300

    The start of block 3 methods

---
+++ src/Task3/_3/Main.java
@@ -0,0 +1,26 @@
+package Task3._3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        printDivider(number);
+    }
+
+    private static void printDivider(int number) {
+
+        // Проверка на натуральное число
+        if (number<=0){
+            System.out.println("Erorr");
+            return;
+        }
+        // Вывод всех делителей натурального числа
+        for (int i =1; i<=number;i++){
+            if (number % i ==0){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
